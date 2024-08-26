b817032909c351fa8bc36d8737c5ba2189798666
devambarbhaya <barbhaya.devam@gmail.com>
2024-08-26 13:40:18 +0530

    add moving up / down

--- player.py
+++ player.py
@@ -29,3 +29,13 @@ class Player(CircleShape):
     
     if keys[pygame.K_d]:
       self.rotate(dt)
+    
+    if keys[pygame.K_w]:
+      self.move(dt)
+      
+    if keys[pygame.K_s]:
+      self.move(-dt)
+      
+  def move(self, dt):
+    forward = pygame.Vector2(0, 1).rotate(self.rotation)
+    self.position += forward * PLAYER_SPEED * dt
