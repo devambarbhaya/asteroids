3bac629226e3e2eb5c9cbadbfb4bc9a92ccf05a6
devambarbhaya <barbhaya.devam@gmail.com>
2024-08-26 13:17:59 +0530

    add FPS

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
   
   while True:
     screen.fill("black")
@@ -12,6 +14,8 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+      
+    dt = clock.tick(60) / 1000
   
 if __name__ == "__main__":
   main()
